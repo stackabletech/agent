4115e5c7f09e9b09f742e7f77b7f4536b3baacbb
Sönke Liebau <soenke.liebau@gmail.com>
2020-12-01 16:47:21 +0100

    Forgot to address comments in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::env;
 use std::ffi::OsString;
-use std::net::IpAddr;
-use std::path::PathBuf;
 
 use kube::config::Config as KubeConfig;
 use kube::config::KubeConfigOptions;
-use kubelet::config::{Config, ServerConfig};
+use kubelet::config::Config;
 use kubelet::Kubelet;
-use log::{debug, info, warn};
+use log::{info, warn};
 use pnet::datalink;
 use stackable_config::ConfigBuilder;
 
@@ -22,19 +20,24 @@ async fn main() -> anyhow::Result<()> {
     // Initialize the logger
     env_logger::init();
 
-    // The provider is responsible for all the "back end" logic. If you are creating
-    // a new Kubelet, all you need to implement is a provider.
-
     let agent_config: AgentConfig =
         ConfigBuilder::build(env::args_os().collect::<Vec<OsString>>(), "CONFIG_FILE")
             .expect("Error initializing Configuration!");
 
+    // Currently the only way to _properly_ configure the Krustlet is via these environment exports,
+    // as their config object only offers methods that parse from command line flags (or combinations
+    // of those flags with other things).
+    // Since we have our own command line flags that are not compatible with the Krustlet's we
+    // configure the agent via a file from the environment variable (CONFIG_FILE), extract what
+    // is needed for the Krustlet and pass it via environment variables.
+    // This is an ugly hack for now, until we've had time to take a proper look at Krustlet's config
     export_env(
         "KRUSTLET_NODE_IP",
         &agent_config.server_ip_address.to_string(),
     );
 
     // Convert node tags to string in the form of key=value,key=value,...
+    // TODO: check for commas in the key value pairs themselves
     let node_labels = agent_config
         .tags
         .iter()
@@ -58,13 +61,10 @@ async fn main() -> anyhow::Result<()> {
     info!("args: {:?}", env::args());
     let krustlet_config = Config::new_from_flags(env!("CARGO_PKG_VERSION"));
 
-    //let kubeconfig = kubelet::bootstrap(&config, &config.bootstrap_file, notify_bootstrap).await?;
     let kubeconfig = KubeConfig::from_kubeconfig(&KubeConfigOptions::default())
         .await
         .expect("Failed to create Kubernetes Client!");
 
-    //let parcel_directory = PathBuf::from("/home/sliebau/IdeaProjects/agent/work/parcels");
-    //let config_directory = PathBuf::from("/home/sliebau/IdeaProjects/agent/work/config");
     let provider = StackableProvider::new(
         kube::Client::new(kubeconfig.clone()),
         agent_config.parcel_directory.clone(),
